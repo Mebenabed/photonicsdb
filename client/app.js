f96c94ca686aa4e0f8a869da08a0ea6f624e5ed7
mhdbnbd <mehdi.benabed@live.fr>
2018-11-19 17:35:50 +0100

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,9 +1,14 @@
+// importing ngRoute directive to provide routing services
 var myApp = angular.module('myApp',['ngRoute']);
-
+//configuring routes by specifying bound controller and view
 myApp.config(function($routeProvider){
 	$routeProvider.when('/', {
 		controller:'MainsController',
-		templateUrl: 'views/mains.html'
+		templateUrl: 'views/home.html'
+	})
+	.when('/home', {
+		controller:'MainsController',
+		templateUrl: 'views/home.html'
 	})
 	.when('/mains', {
 		controller:'MainsController',
@@ -53,13 +58,37 @@ myApp.config(function($routeProvider){
 			controller:'MainsController',
 			templateUrl: 'views/component.html'
 	})
+	.when('/assign_component/:id',{
+			controller:'MainsController',
+			templateUrl: 'views/assign_component.html'
+	})
 	.when('/component/edit/:id',{
 			controller:'MainsController',
 			templateUrl: 'views/edit_component.html'
 	})
+	.when('/addf',{
+			controller:'MainsController',
+			templateUrl: 'views/addf.html'
+	})
+	.when('/addc',{
+			controller:'MainsController',
+			templateUrl: 'views/addc.html'
+	})
+	.when('/addb',{
+			controller:'MainsController',
+			templateUrl: 'views/addb.html'
+	})
 	.when('/add',{
 			controller:'MainsController',
 			templateUrl: 'views/add.html'
+	})
+	.when('/insights',{
+			controller:'MainsController',
+			templateUrl: 'views/insights.html'
+	})
+	.when('/scopes',{
+			controller:'MainsController',
+			templateUrl: 'views/scopes.html'
 	})
 	.otherwise({
 		redirectTo: '/'
